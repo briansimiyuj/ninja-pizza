e6c7d6e48550273bb70be8976b34a418a091675e
Brian Simiyu <briansimiyuj@gmail.com>
2024-08-12 10:05:13 +0300

    Form mark up

---
+++ add.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php include "templates/header.php"; ?>
+
+    <section class="container grey-text">
+        
+        <h4 class="center">Add a Pizza</h4>
+
+
+        <form class="white" action="" method="">
+
+            <label for="email">Your Email:</label>
+
+            <input type="text" name="email">
+
+            <label for="title">Pizza Title:</label>
+
+            <input type="text" name="title">
+            
+            <label for="ingredients">Ingredients (comma separated):</label>
+
+            <input type="text" name="ingredients">
+
+
+            <div class="center">
+
+                <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+
+            </div>
+
+        </form>
+    
+    </section>
+
+    <?php include "templates/footer.php"; ?>
+    
+</html>
--- templates/header.php
+++ templates/header.php
@@ -13,6 +13,12 @@
             color: #cbb09c !important;
         }
 
+        form{
+            max-width: 460px;
+            margin: 20px auto;
+            padding: 20px;
+        }
+
     </style>
 </head>
 
